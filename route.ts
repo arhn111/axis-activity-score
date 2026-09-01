f4f95a1690540007f199a3380d4bbfcb3e99a0a3
Arhn <146503162+arhn111@users.noreply.github.com>
2026-09-01 11:09:25 +0530

    Update route.ts

--- route.ts
+++ route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getWalletData } from "@/lib/axis";
+import { getWalletData } from "@/axis";
 
 export async function GET(req: NextRequest) {
   try {
